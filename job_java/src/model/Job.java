d0203eb3a94e3c2db0454ef4c5c4d0f981bcc2c6
peterhadda <104314694+peterhadda@users.noreply.github.com>
2026-02-12 19:29:06 -0800

    Update Job.java

--- job_java/src/model/Job.java
+++ job_java/src/model/Job.java
@@ -14,8 +14,23 @@ public class Job {
         this.jobId=jobId;
         this.title=title;
         this.company=company;
-
-
+        this.location=location;
+        this.datePosted=datePosted;
+        this.description=description;
+        this.url=url;
+        this.source=source;
     }
+
+    public string getJobId(){
+        return jobId;
+    }
+    public string getTitle(){
+        return title;
+    }
+    
+
+
+
+
 
 }
